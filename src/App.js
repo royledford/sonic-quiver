3c31816b90cc27453d8ac6b0ca5d8a4866c463e5
roy ledford <bluesixty@gmail.com>
2017-11-27 15:23:45 -0500

    Remove testhtml stuff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Play from './Components/Pages/Play'
 import HomePage from './Components/Pages/HomePage'
-import TestHtmlElements from './TestHtmlElements'
 import RecentsPage from './Components/Pages/RecentsPage'
 import AboutPage from './Components/Pages/AboutPage'
 import WorldwidePage from './Components/Pages/WorldwidePage'
 import LibraryPage from './Components/Pages/LibraryPage'
 
 import './App.css'
-
 
 class App extends Component {
   render() {
@@ -23,7 +22,6 @@ class App extends Component {
           <Route exact path="/library" component={LibraryPage} />
 
           <Route exact path="/play" component={Play} />
-          <Route exact path="/testhtml" component={TestHtmlElements} />
         </Switch>
       </Router>
     )
