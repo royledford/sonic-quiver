454e50b4ac8bb8f03c3513232e0524d9df740a44
Roy Ledford <bluesixty@gmail.com>
2017-11-27 15:25:19 -0500

    Remove testhtml stuff (#20)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
 import './vendor/normalize.scss'
 import './vendor/reset.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
