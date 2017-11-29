8bb796779d205e07ff68adc2d2a1521d2d03ab9c
roy ledford <bluesixty@gmail.com>
2017-11-29 11:27:58 -0500

    Fix issue with worldwide page on mobile

--- src/Components/Agents/AgentSmall.js
+++ src/Components/Agents/AgentSmall.js
@@ -4,9 +4,17 @@ import './AgentSmall.css'
 
 export default class AgentSmall extends Component {
   static propTypes = {
-    location: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired,
     companyName: PropTypes.string.isRequired,
     agentName: PropTypes.string.isRequired,
+    address1: PropTypes.string,
+    address2: PropTypes.string,
+    address3: PropTypes.string,
+    phone: PropTypes.string,
+    fax: PropTypes.string,
+    email: PropTypes.string,
+    urlDisplay: PropTypes.string,
+    sonicUrl: PropTypes.string,
   }
 
   handleHover = () => {
@@ -14,14 +22,50 @@ export default class AgentSmall extends Component {
   }
 
   render() {
-    const { location, companyName, agentName } = this.props
+    const {
+      country,
+      companyName,
+      agentName,
+      email,
+      address1,
+      address2,
+      address3,
+      phone,
+      fax,
+      urlDisplay,
+      sonicUrl,
+    } = this.props
+
+    const faxDisplay = fax ? `FAX: ${fax}` : ''
+
     return (
       <div className="agentsmall-wrap" onMouseEnter={this.handleHover}>
-        <h4 className="agentsmall-location">{location}</h4>
+        <h4 className="country">{country}</h4>
         <hr className="agentsmall-rule" />
-        <p className="agentsmall-company">
+        <p className="agentsmall-copy">
           {companyName} - {agentName}
         </p>
+        <div className="agentsmall-mobile">
+          <p>
+            <a href={`mailto:${email}`}>{email}</a>
+          </p>
+          <a href={sonicUrl} target="_blank" rel="noopener noreferrer">
+            {urlDisplay}
+          </a>
+          <div className="agentsmall-copy">
+            TEL: {phone}
+            {fax && <br />}
+            {faxDisplay}
+          </div>
+          <div className="agentsmall-copy">
+            {address1}
+            <br />
+            {address2}
+            <br />
+            {address3}
+            <br />
+          </div>
+        </div>
       </div>
     )
   }
