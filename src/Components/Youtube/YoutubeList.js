c7d91cbc09f8a4369f22cea28cf3ab8aaec09eee
roy ledford <bluesixty@gmail.com>
2017-12-01 10:38:00 -0500

    Add youtube loading

--- src/Components/Youtube/YoutubeList.js
+++ src/Components/Youtube/YoutubeList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import YoutubeService from '../../api/youtubeApi'
 import YoutubeSmall from './YoutubeSmall'
+import YoutubeSmallLoader from './YoutubeSmallLoader'
 import YoutubePlayer from './YoutubePlayer'
 import './YoutubeList.css'
 
@@ -14,7 +15,7 @@ export default class YoutubeList extends Component {
     super(props)
     this.state = {
       ytVids: [],
-      loading: false,
+      loading: true,
       videoToPlay: '',
       videoTitle: '',
       showVideo: false,
@@ -45,25 +46,31 @@ export default class YoutubeList extends Component {
   }
 
   render() {
-    const { videoToPlay, videoTitle, showVideo, ytVids } = this.state
+    const { videoToPlay, videoTitle, showVideo, ytVids, loading } = this.state
+    let ytList = []
+    if (loading) {
+      ytList = [1, 2, 3].map((_, i) => <YoutubeSmallLoader key={i} />)
+    } else {
+      ytList = ytVids.map(
+        vid =>
+          vid.id.videoId ? (
+            <YoutubeSmall
+              key={vid.id.videoId}
+              id={vid.id.videoId}
+              title={vid.snippet.title}
+              description={vid.snippet.description}
+              thumbnailUrl={vid.snippet.thumbnails.high.url}
+              playVideo={this.handlePlayClicked}
+            />
+          ) : null
+      )
+    }
+
     return (
       <div className="youtubelist-wrap">
+        {/* <YoutubeSmallLoader id="loader" /> */}
         <div className="youtubelist-row">
-          <div className="youtubelist-list">
-            {ytVids.map(
-              vid =>
-                vid.id.videoId ? (
-                  <YoutubeSmall
-                    key={vid.id.videoId}
-                    id={vid.id.videoId}
-                    title={vid.snippet.title}
-                    description={vid.snippet.description}
-                    thumbnailUrl={vid.snippet.thumbnails.high.url}
-                    playVideo={this.handlePlayClicked}
-                  />
-                ) : null
-            )}
-          </div>
+          <div className="youtubelist-list">{ytList}</div>
         </div>
         <YoutubePlayer
           videoId={videoToPlay}
